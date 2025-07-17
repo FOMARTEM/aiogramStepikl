ec369b28ccff0e422242401432e29d738a0dfd9d
Artem Shahov <shahov9ad@gmail.com>
2025-07-18 00:20:45 +0300

    command start

--- 8.3-number/main.py
+++ 8.3-number/main.py
@@ -11,6 +11,16 @@ bot = Bot(token=token)
 dp = Dispatcher()
 
 
+@dp.message(Command(commands='start'))
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer('Привет! Я бот "Угадай Число".'
+                         'Предлагаю поиграть со мной)\n'
+                         'Отправь /help что бы ознакомиться с правилами'
+    )
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
